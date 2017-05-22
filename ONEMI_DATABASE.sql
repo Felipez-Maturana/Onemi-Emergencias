c73e739ca84ebaad80b1df1ba7c8e9a1611401e3
felipez-maturana <felipe.maturana.g@usach.cl>
2017-05-22 03:44:31 -0400

    trigger

--- ONEMI_DATABASE.sql
+++ ONEMI_DATABASE.sql
@@ -2,58 +2,59 @@
 /* Table: MURO                                                  */
 /*==============================================================*/
 create table MURO (
-    ID_MURO          	INT4                 not null,
-    primary key (ID_MURO)
+    ID          	INT4                 not null,
+    primary key (ID)
 );
 	
 /*==============================================================*/
 /* Table: USUARIO                                               */
 /*==============================================================*/
 create table USUARIO (
-   ID_USUARIO           INT4                 not null,
-   USUARIO              VARCHAR(25)          null,
-   PASSWORD             VARCHAR(15)          null,
+   ID           INT4                 not null,
+   USUARIO              VARCHAR(25)          not null,
+   PASSWORD             VARCHAR(15)          not null,
    TIPO_USUARIO         INT4                 null,
    RUN                  INT4                 null,
    EMAIL                VARCHAR(40)          null,
    TELEFONO             INT4                 null,
-   ACTIVO 				BOOLEAN				 null,
-   primary key (ID_USUARIO)
+   ACTIVO 			       	BOOLEAN	       			 not null,
+   VISIBLE              BOOLEAN              not null,
+   primary key (ID)
 );
 
 /*==============================================================*/
 /* Table: HISTORIAL                                             */
 /*==============================================================*/
 create table HISTORIAL (
-   ID_HISTORIAL          	INT4                 not null,
+   ID          	INT4                 not null,
    ID_USUARIO				INT4                 not null,
    FECHA 					DATE 				 not null,
    HORA 					TIME 				 not null,
-   primary key (ID_HISTORIAL),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO)
+   primary key (ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID)
 );
 
 /*==============================================================*/
 /* Table: CATASTROFE                                            */
 /*==============================================================*/
 create table CATASTROFE (
-   ID_CATASTROFE        INT4                 not null,
+   ID        INT4                 not null,
    ID_USUARIO           INT4                 null,
    DESCRIPCION	        VARCHAR(20)          null,
    REGION               VARCHAR(15)          null,
    COMUNA               VARCHAR(15)          null,
-   primary key (ID_CATASTROFE),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO)
+   primary key (ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID)
 );
 
 /*==============================================================*/
 /* Table: EVENTO                                                */
 /*==============================================================*/
 create table EVENTO (
-   ID_EVENTO            INT4                 not null,
+   ID            INT4                 not null,
    ID_CATASTROFE        INT4                 not null,
    ID_USUARIO           INT4                 not null,
-   ID_MURO          	INT4                 null,
+   ID_MURO             	INT4                 null,
    NOMBRE_MEDIDA        VARCHAR(15)          null,
    FECHA_INICIO         DATE                 null,
    FECHA_TERMINO        DATE                 null,
@@ -62,33 +63,33 @@ create table EVENTO (
    FECHA                DATE                 null,
    HORA                 TIME                 null,
    MONTO_RECAUDADO      INT4                 null,
-   primary key (ID_EVENTO),
-   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID_CATASTROFE),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO),
-   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID_MURO)
+   primary key (ID),
+   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID),
+   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID)
 );
 
 /*==============================================================*/
 /* Table: ACTIVIDAD                                             */
 /*==============================================================*/
 create table ACTIVIDAD (
-   ID_ACTIVIDAD         INT4                 not null,
-   ID_EVENTO			INT4				 not null,
+   ID         INT4                 not null,
+   ID_EVENTO	       		INT4		        		 not null,
    ID_USUARIO           INT4                 not null,
    DESCRIPCION          VARCHAR(100)         null,
-   PRIMARY KEY (ID_ACTIVIDAD),
-   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID_EVENTO),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO)
+   PRIMARY KEY (ID),
+   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID)
 );
 
 /*==============================================================*/
 /* Table: VOLUNTARIADO                                          */
 /*==============================================================*/
 create table VOLUNTARIADO (
-   ID_VOLUNTARIADO      INT4                 not null,
+   ID      INT4                 not null,
    ID_CATASTROFE        INT4                 not null,
    ID_USUARIO           INT4                 not null,
-   ID_MURO          	INT4                 not null,
+   ID_MURO             	INT4                 not null,
    NOMBRE_MEDIDA        VARCHAR(15)          null,
    FECHA_INICIO         DATE                 null,
    FECHA_TERMINO        DATE                 null,
@@ -97,52 +98,53 @@ create table VOLUNTARIADO (
    PERSONAS             INT4                 null,
    TIPO_TRABAJO         VARCHAR(20)          null,
    DIRECCION            VARCHAR(50)          null,
-   primary key (ID_VOLUNTARIADO),
-   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID_CATASTROFE),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO),
-   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID_MURO)
+   primary key (ID),
+   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID),
+   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID)
 );
 
 /*==============================================================*/
 /* Table: CUENTA_BANCO                                          */
 /*==============================================================*/
 create table CUENTA_BANCO (
-   ID_CUENTA            INT4                 not null,
+   ID                   INT4                 not null,
    NUMERO_CUENTA        INT4                 null,
    BANCO                VARCHAR(20)          null,
    TIPO_CUENTA          INT4                 null,
    RUN                  INT4                 null,
-   primary key (ID_CUENTA)
+   primary key (ID)
 );
 
 /*==============================================================*/
 /* Table: RECOLECCION                                           */
 /*==============================================================*/
 create table RECOLECCION (
-   ID_RECOLECCION       INT4                 not null,
+   ID                   INT4                 not null,
    ID_CATASTROFE        INT4                 null,
    ID_USUARIO           INT4                 null,
-   ID_MURO          	INT4                 not null,
+   ID_MURO             	INT4                 not null,
    NOMBRE_MEDIDA        VARCHAR(15)          null,
    FECHA_INICIO         DATE                 null,
    FECHA_TERMINO        DATE                 null,
    AVANCE               DECIMAL              null,
    DIRECCION            VARCHAR(50)          null,
-   primary key (ID_RECOLECCION),
-   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID_CATASTROFE),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO),
-   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID_MURO)
+   ELEMENTOSNECESARIOS  INT4                 not null,
+   primary key (ID),
+   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID),
+   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID)
 );
 
 /*==============================================================*/
 /* Table: APOYO_ECONOMICO                                       */
 /*==============================================================*/
 create table APOYO_ECONOMICO (
-   ID_APOYO             INT4                 not null,
+   ID             INT4                 not null,
    ID_CUENTA            INT4                 null,
    ID_CATASTROFE        INT4                 null,
    ID_USUARIO           INT4                 null,
-   ID_MURO          	INT4                 not null,
+   ID_MURO            	INT4                 not null,
    NOMBRE_MEDIDA        VARCHAR(15)          null,
    FECHA_INICIO         DATE                 null,
    FECHA_TERMINO        DATE                 null,
@@ -150,11 +152,11 @@ create table APOYO_ECONOMICO (
    META                 INT4                 null,
    RECAUDACION_ACTUAL   INT4                 null,
 
-   primary key (ID_APOYO),
-   FOREIGN KEY (ID_CUENTA) REFERENCES CUENTA_BANCO(ID_CUENTA),
-   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID_CATASTROFE),
-   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO),
-   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID_MURO)
+   primary key (ID),
+   FOREIGN KEY (ID_CUENTA) REFERENCES CUENTA_BANCO(ID),
+   FOREIGN KEY (ID_CATASTROFE) REFERENCES CATASTROFE(ID),
+   FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID),
+   FOREIGN KEY (ID_MURO) REFERENCES MURO(ID)
 );
 
 
@@ -163,7 +165,7 @@ create table APOYO_ECONOMICO (
 /* Table: PERSONA                                               */
 /*==============================================================*/
 create table PERSONA(
-	ID_PERSONA			INT4				 not null,	
+	  ID			INT4				 not null,	
     ID_RECOLECCION      INT4                 null,
     ID_APOYO            INT4                 null,
     ID_EVENTO           INT4                 null,
@@ -172,87 +174,132 @@ create table PERSONA(
     NOMBRE              VARCHAR(20)          not null,
     APELLIDO_PATERNO    VARCHAR(20)          not null,
     APELLIDO_MATERNO    VARCHAR(20)          not null, 
-    primary key (ID_PERSONA),
-    FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID_RECOLECCION),
-    FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID_APOYO),
-    FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID_EVENTO),
-    FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID_VOLUNTARIADO)
+    primary key (ID),
+    FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID),
+    FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID),
+    FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID),
+    FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID)
 );
 
 /*==============================================================*/
 /* Table: DONACION                                              */
 /*==============================================================*/
 create table DONACION(
-	ID_DONACION			INT4				 not null,
+	ID			INT4				 not null,
 	ID_PERSONA			INT4				 not null,	
 	ID_CUENTA			INT4				 not null,	
 	HORA				TIME 				 not null,
 	FECHA				DATE 				 not null,
 	MONTO 				MONEY				 not null,
-	primary key (ID_DONACION),
-	FOREIGN KEY (ID_PERSONA) REFERENCES PERSONA(ID_PERSONA),
-	FOREIGN KEY (ID_CUENTA) REFERENCES CUENTA_BANCO(ID_CUENTA)
+	primary key (ID),
+	FOREIGN KEY (ID_PERSONA) REFERENCES PERSONA(ID),
+	FOREIGN KEY (ID_CUENTA) REFERENCES CUENTA_BANCO(ID)
 );
 
 /*==============================================================*/
 /* Table: ELEMENTOS                                             */
 /*==============================================================*/
 create table ELEMENTOS (
-   ID_ELEMENTO          INT4                 not null,
+   ID          INT4                 not null,
    ID_RECOLECCION       INT4                 not null,
    NOMBRE               VARCHAR(15)          null,
    CANTIDAD             INT4                 null,
-   primary key (ID_ELEMENTO),
-   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID_RECOLECCION)
+   primary key (ID),
+   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID)
 );
 
 /*==============================================================*/
 /* Table: MATERIALES                                            */
 /*==============================================================*/
 create table MATERIALES (
-   ID_MATERIAL          INT4                 not null,
+   ID          INT4                 not null,
    ID_RECOLECCION       INT4                 null,
    ID_APOYO             INT4                 null,
    ID_EVENTO            INT4                 null,
    ID_VOLUNTARIADO      INT4                 null,
    NOMBRE               VARCHAR(15)          null,
    CANTIDAD             INT4                 null,
-   primary key (ID_MATERIAL),
-   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID_RECOLECCION),
-   FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID_APOYO),
-   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID_EVENTO),
-   FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID_VOLUNTARIADO)
+   primary key (ID),
+   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID),
+   FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID),
+   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID),
+   FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID)
 );
 
 /*==============================================================*/
 /* Table: OBJETIVO_DE_AYUDA                                     */
 /*==============================================================*/
 create table OBJETIVO_DE_AYUDA (
-   ID_OBJETIVO          INT4                 not null,
+   ID          INT4                 not null,
    ID_RECOLECCION       INT4                 null,
    ID_APOYO             INT4                 null,
    ID_EVENTO            INT4                 null,
    ID_VOLUNTARIADO      INT4                 null,
    DESCRIPCION          VARCHAR(100)         null,
-   primary key (ID_OBJETIVO),
-   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID_RECOLECCION),
-   FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID_APOYO),
-   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID_EVENTO),
-   FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID_VOLUNTARIADO)
+   primary key (ID),
+   FOREIGN KEY (ID_RECOLECCION) REFERENCES RECOLECCION(ID),
+   FOREIGN KEY (ID_APOYO) REFERENCES APOYO_ECONOMICO(ID),
+   FOREIGN KEY (ID_EVENTO) REFERENCES EVENTO(ID),
+   FOREIGN KEY (ID_VOLUNTARIADO) REFERENCES VOLUNTARIADO(ID)
 );
 
 /*==============================================================*/
 /* Table: COMENTARIO                                            */
 /*==============================================================*/
 create table COMENTARIO (
-	ID_COMENTARIO		INT4				 not null,
+  	ID		INT4				 not null,
     ID_MURO          	INT4                 not null,
     ID_USUARIO           INT4 				 not null,
     DESCRIPCION			VARCHAR(140)		 not null,
-    primary key (ID_COMENTARIO),
-    FOREIGN KEY (ID_MURO) REFERENCES MURO(ID_MURO),
-    FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID_USUARIO) 
+    primary key (ID),
+    FOREIGN KEY (ID_MURO) REFERENCES MURO(ID),
+    FOREIGN KEY (ID_USUARIO) REFERENCES USUARIO(ID) 
 );
+
+
+/*==============================================================*/
+/* Trigger: noborrar()                                          */
+/*==============================================================*/
+
+CREATE OR REPLACE FUNCTION noborrar()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+  UPDATE usuario SET visible=false WHERE id=OLD.id;
+  RETURN NULL;
+END; $BODY$
+  LANGUAGE plpgsql
+
+CREATE TRIGGER delete_trg
+  BEFORE DELETE
+  ON usuario
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.noborrar();
+
+
+
+
+
+
+
+
+
+
+-- *********************************************************************FUNCTIONS
+-- CREATE FUNCTION clienteDelete(integer) RETURNS TRIGGER AS $_$
+-- BEGIN
+--     UPDATE usuario SET activo=0 WHERE usuario_id = $1
+-- END $_$ LANGUAGE 'plpgsql';
+
+
+
+
+
+
+
+
+
+
 
 
 /*==============================================================*/
